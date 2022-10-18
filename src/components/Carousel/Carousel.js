f61f7fc6f02437ea5a762f61dd90146e77023796
김도영 <go3152@gmail.com>
2022-10-18 20:01:20 +0900

    Modify: 스크롤 구현,버튼 호버 구현

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import CarouselItem from "./CarouselItem.js";
 import "./Carousel.scss";
 import { CarouselItemData } from "./CarouselItemData.js";
 
 function Carousel() {
+  const [scrollX, setScrollX] = useState("");
+
+  // function carouselButtonClick() {
+  //   console.log(window.scrollX);
+  //   setScrollX(setScrollX + 100);
+  // carousel.pageXoffset;
+  // }
+
   return (
     <div className="carousel-container">
-      <button className="carousel-button-left button-common-css">{`<`}</button>
+      <button
+        // onClick={carouselButtonClick}
+        className="carousel-button-left button-common-css"
+      >{`<`}</button>
       <div className="carousel">
         {CarouselItemData.map(item => {
           return (
@@ -16,7 +27,10 @@ function Carousel() {
           );
         })}
       </div>
-      <button className="carousel-button-right button-common-css">{`>`}</button>
+      <button
+        // onClick={carouselButtonClick}
+        className="carousel-button-right button-common-css"
+      >{`>`}</button>
     </div>
   );
 }
